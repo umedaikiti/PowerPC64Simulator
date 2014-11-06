8abc5bb21c4af3625e582aecce56cbd7189686f5
umedaikiti <umedaikiti@gmail.com>
2014-11-06 15:22:54 +0900

    クイックソートのバグを修正

--- test/qsort/qsort.c
+++ test/qsort/qsort.c
@@ -1,11 +1,21 @@
 void qsort100(int *a, unsigned int n)
 {
-	int m, i;
+	int m;
 	int a1[100];
 	int a2[100];
-	int i1 = 0, i2 = 0;
-	if(n <= 1) return;
-	m = a[0] > a[1] ? a[0] : a[1];
+	unsigned int i, i1 = 0, i2 = 0;
+
+	m = a[0];
+	for(i=1;i<n;i++){
+		if(a[i] != m){
+			if(a[i] > m){
+				m = a[i];
+			}
+			break;
+		}
+	}
+	if(i == n) return;
+
 	for(i=0;i<n;i++){
 		if(a[i] < m){
 			a1[i1++] = a[i];
@@ -14,10 +24,9 @@ void qsort100(int *a, unsigned int n)
 			a2[i2++] = a[i];
 		}
 	}
-	if(i1 != 0){
-		qsort100(a1, i1);
-		qsort100(a2, i2);
-	}
+	qsort100(a1, i1);
+	qsort100(a2, i2);
+
 	for(i=0;i<i1;i++){
 		a[i] = a1[i];
 	}
@@ -30,11 +39,10 @@ int test1[] = {2, -1, 3, 4, 2, 1, 1, 5};
 int test2[] = {-1, 0};
 int test3[] = {2, 2};
 int test4[] = {3, 2};
-int test5[] = {3, 2, 1};
+int test5[] = {1, 1, 4, 3, 2};
 
 int main()
 {
-	int i;
 	qsort100(test1, sizeof(test1)/sizeof(test1[0]));
 	qsort100(test2, sizeof(test2)/sizeof(test2[0]));
 	qsort100(test3, sizeof(test3)/sizeof(test3[0]));
