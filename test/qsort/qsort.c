df22ef7918827746900d583fcc42eeef1123b83f
umedaikiti <umedaikiti@gmail.com>
2014-11-13 15:27:20 +0900

    クイックソート修正

--- test/qsort/qsort.c
+++ test/qsort/qsort.c
@@ -1,38 +1,30 @@
-void qsort100(int *a, unsigned int n)
+void qsort(int a[], int n, int m)
 {
-	int m;
-	int a1[100];
-	int a2[100];
-	unsigned int i, i1 = 0, i2 = 0;
+	int p = a[n];
+	int i, j;
 
-	m = a[0];
-	for(i=1;i<n;i++){
-		if(a[i] != m){
-			if(a[i] > m){
-				m = a[i];
-			}
+	if (m - n <= 1) return;
+
+	for(i=n+1;i<m;i++){
+		if(p != a[i]){
+			if(p < a[i]) p = a[i];
 			break;
 		}
 	}
-	if(i == n) return;
-
-	for(i=0;i<n;i++){
-		if(a[i] < m){
-			a1[i1++] = a[i];
-		}
-		else{
-			a2[i2++] = a[i];
-		}
+	if(i == m) return;
+	i = n;
+	j = m-1;
+	for(;;){
+		int tmp;
+		while(a[i] < p) i++;
+		while(a[j] >= p) j--;
+		if(i >= j) break;
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
 	}
-	qsort100(a1, i1);
-	qsort100(a2, i2);
-
-	for(i=0;i<i1;i++){
-		a[i] = a1[i];
-	}
-	for(i=0;i<i2;i++){
-		a[i1+i] = a2[i];
-	}
+	qsort(a, n, i-1);
+	qsort(a, i, m);
 }
 
 int test1[] = {2, -1, 3, 4, 2, 1, 1, 5};
@@ -43,10 +35,10 @@ int test5[] = {1, 1, 4, 3, 2};
 
 int main()
 {
-	qsort100(test1, sizeof(test1)/sizeof(test1[0]));
-	qsort100(test2, sizeof(test2)/sizeof(test2[0]));
-	qsort100(test3, sizeof(test3)/sizeof(test3[0]));
-	qsort100(test4, sizeof(test4)/sizeof(test4[0]));
-	qsort100(test5, sizeof(test5)/sizeof(test5[0]));
+	qsort(test1, 0, sizeof(test1)/sizeof(test1[0]));
+	qsort(test2, 0, sizeof(test2)/sizeof(test2[0]));
+	qsort(test3, 0, sizeof(test3)/sizeof(test3[0]));
+	qsort(test4, 0, sizeof(test4)/sizeof(test4[0]));
+	qsort(test5, 0, sizeof(test5)/sizeof(test5[0]));
 	return 0;
 }
